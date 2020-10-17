dc1c52eaf93460f0569a525f059bbc34a75adaa0
Maxim <lostrandg@gmail.com>
2020-10-17 08:04:09 +0500

    update project sources

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,4 @@
-import database.model.User;
-import database.services.UserService;
-
 public class Main {
     public static void main(String[] args) {
-        UserService userService = new UserService();
-        User user = new User("name", "pass");
-        userService.saveUser(user);
     }
 }
