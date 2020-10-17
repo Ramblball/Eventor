ed60271ae7173dcf6de69c8a3de5762f5d7802fa
Maxim <lostrandg@gmail.com>
2020-10-17 07:58:08 +0500

    update Event structure

--- src/main/java/database/model/Event.java
+++ src/main/java/database/model/Event.java
@@ -1,101 +1,103 @@
 package database.model;
 
+import org.hibernate.annotations.Type;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table(name = "event")
+@Table(name = "event", schema = "eventor_schema")
 public class Event {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private int organizer;
+    @Column(name = "user_id")
+    private int userId;
     private String name;
     private String place;
     private LocalDateTime time;
-    @Column(name = "category")
-    private int category_id;
     private String description;
 
-    @ManyToOne
-    @JoinColumn(name = "category_id")
+    @Enumerated(EnumType.STRING)
+    @Type(type = "database.type.EnumTypePostgreSQL")
     private Category category;
-
     @ManyToOne
-    @JoinColumn(name = "organizer", insertable = false, updatable = false)
-    private User orgUser;
+    @JoinColumn(name = "user_id", insertable = false, updatable = false)
+    private User user;
 
-    @ManyToMany(mappedBy = "subEvents")
+    @ManyToMany(mappedBy = "subscribes")
     private Set<User> subscribers = new HashSet<>();
 
     public Event() {
     }
 
-    public Event(int organizer, String name, String place, LocalDateTime time, int category_id, String description) {
-        this.organizer = organizer;
+    public Event(String name, String place, LocalDateTime time, Category category, String description) {
         this.name = name;
         this.place = place;
         this.time = time;
-        this.category_id = category_id;
+        this.category = category;
         this.description = description;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Set<User> getSubscribers() {
+        return subscribers;
+    }
+    public void setSubscribers(Set<User> subscribers) {
+        this.subscribers = subscribers;
     }
 
     public int getId() {
         return id;
     }
 
-    public int getOrganizer() {
-        return organizer;
-    }
-
     public String getName() {
         return name;
+    }
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getPlace() {
         return place;
     }
+    public void setPlace(String place) {
+        this.place = place;
+    }
 
     public LocalDateTime getTime() {
         return time;
     }
-
-    public int getCategory_id() {
-        return category_id;
+    public void setTime(LocalDateTime time) {
+        this.time = time;
     }
 
     public String getDescription() {
         return description;
     }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
     public Category getCategory() {
         return category;
     }
-
-    public User getOrgUser() {
-        return orgUser;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPlace(String place) {
-        this.place = place;
-    }
-
-    public void setTime(LocalDateTime time) {
-        this.time = time;
-    }
-
-    public void setCategory_id(int category_id) {
-        this.category_id = category_id;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
+    public void setCategory(Category category) {
+        this.category = category;
     }
 }
