fb2bd22a211d6b513771cd4ccfb10425e5ed922b
Maxim <lostrandg@gmail.com>
2020-11-06 01:22:52 +0500

    add javaDoc to services

--- src/main/java/database/services/EventService.java
+++ src/main/java/database/services/EventService.java
@@ -8,14 +8,34 @@ import java.util.List;
 public class EventService {
     EventDAOImpl EventDAO = new EventDAOImpl();
 
+    /**
+     * Return Event by provided ID
+     *
+     * @param id  the id of the event
+     * @return    founded event
+     * @see       Event
+     */
     public Event findEventById(int id) {
         return EventDAO.findById(id);
     }
 
+    /**
+     * Return Event by provided Name
+     *
+     * @param name  the name of the event
+     * @return      founded event
+     * @see         Event
+     */
     public Event findEventByName(String name) {
         return EventDAO.findByName(name);
     }
 
+    /**
+     * return all Events
+     *
+     * @return      list of all events
+     * @see         Event
+     */
     public List<Event> findAll() {
         return EventDAO.findAll();
     }
