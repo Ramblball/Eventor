6b6320936f9b795ec286ef8f5ec449c20faedc02
Maxim <lostrandg@gmail.com>
2020-11-12 14:42:12 +0500

    update add error handling

--- src/main/java/database/services/EventService.java
+++ src/main/java/database/services/EventService.java
@@ -11,32 +11,44 @@ public class EventService {
     /**
      * Return Event by provided ID
      *
-     * @param id  the id of the event
-     * @return    founded event
-     * @see       Event
+     * @param id the id of the event
+     * @return founded event
+     * @see Event
      */
     public Event findEventById(int id) {
-        return EventDAO.findById(id);
+        try {
+            return EventDAO.findById(id);
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     /**
      * Return Event by provided Name
      *
-     * @param name  the name of the event
-     * @return      founded event
-     * @see         Event
+     * @param name the name of the event
+     * @return founded event
+     * @see Event
      */
     public Event findEventByName(String name) {
-        return EventDAO.findByName(name);
+        try {
+            return EventDAO.findByName(name);
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     /**
      * return all Events
      *
-     * @return      list of all events
-     * @see         Event
+     * @return list of all events
+     * @see Event
      */
     public List<Event> findAll() {
-        return EventDAO.findAll();
+        try {
+            return EventDAO.findAll();
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
