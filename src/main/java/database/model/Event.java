1cf414dc846c808bbd0305af7837f577823e5fe7
Tempest <Misha8936@yandex.ru>
2020-12-21 10:26:05 +0500

    feature(all): добавление отображения дней до начала мероприятия

--- src/main/java/database/model/Event.java
+++ src/main/java/database/model/Event.java
@@ -8,6 +8,7 @@ import view.TelegramBot;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
 
 import static database.DBLiterals.*;
@@ -167,6 +168,7 @@ public class Event {
         joiner.add("<a href=\"https://api.telegram.org/bot" + TelegramBot.getToken() + "/sendlocation?chat_id=" +
                 getUserId() + "&latitude=" + getLatitude() + "&longitude=" + getLongitude() + "\">Место мероприятия</a>");
         joiner.add("Осталось " + (getLimit() - getSubscribers().size()) + " мест");
+        joiner.add("Осталось дней до начала: " + ChronoUnit.DAYS.between(getTime(), LocalDateTime.now()));
         joiner.add(getDescription());
         return joiner.toString();
     }
